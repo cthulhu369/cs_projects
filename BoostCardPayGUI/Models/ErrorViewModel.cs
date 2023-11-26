967a85be39ba4ddee83c9afb696269b480a47c30
ericp <ericp@fedora>
2023-11-26 08:08:39 -0500

    added mvc gui project

---
+++ BoostCardPayGUI/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BoostCardPayGUI.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
